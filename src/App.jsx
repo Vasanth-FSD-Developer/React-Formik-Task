bd545a7c2732955504d770057a8574a0d1df1768
Vasanth-FSD-Developer <vasanthkumarmba11@gmail.com>
2024-03-16 14:15:00 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppRoutes from './components/utils/AppRoutes'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Footer from './Footer';
 
 const App = () => {
   const router = createBrowserRouter(AppRoutes)
   return (
-    <><RouterProvider router={router} /></>
+    <><RouterProvider router={router} />
+    <Footer/>
+    </>
   )
 }
 
